3b7035029f214cf1e3b4d4d5a05b63c7606c2f00
Tjewell <tjewell@yahoo.com>
2016-03-12 14:31:35 -0500

    My original test commit of cachematrix files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will computing the inverse of a square matrix
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  MatrixInv <- NULL ## set inversion matrix value to null
+  
+  set <- function(y)
+  {
+    x <<- y
+    MatrixInv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(i) MatrixInv <<- solve(x)
+  getInverse <- function() MatrixInv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+  MatrixInv <- x$getInverse()
+  if(!is.null(MatrixInv))##check for blank matrix
+    {
+      message("getting matrix from cash instead of re-inverting")
+      return(MatrixInv) 
+    }
+  
+  MatrixInv <- solve(x$get())
+  x$setInverse(MatrixInv)
+  MatrixInv
+  
 }
+
